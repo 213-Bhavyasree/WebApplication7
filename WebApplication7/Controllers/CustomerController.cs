d58e07f2f29c0f00c2b4249cb3456f5a6a06e8fc
Admin <kloklokesh846@gmail.com>
2021-12-31 15:02:50 +0530

    Add project files.

---
+++ WebApplication7/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Customer> customers = new List<Customer>();
+            customers.Add(new Customer() { ID = 213, CustomerName = "Bhavya", CustomerAddress = "Kadapa" });
+            customers.Add(new Customer() { ID = 214, CustomerName = "Kavya", CustomerAddress = "guntur" });
+            customers.Add(new Customer() { ID = 215, CustomerName = "lavanvya", CustomerAddress = "nellore" });
+
+            ViewData["customers"] = customers;
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication7/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+    }
+}
